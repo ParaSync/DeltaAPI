800a50cfbc1f808b20b264bf94a3389b5fbdbff3
Pyro <pyroslayzrsc@gmail.com>
2025-10-09 11:21:54 +0800

    sample

---
+++ src/models/forms.ts
@@ -0,0 +1,34 @@
+type HTMLInputType =
+  | "button"
+  | "checkbox"
+  | "color"
+  | "date"
+  | "datetime-local"
+  | "email"
+  | "file"
+  | "hidden"
+  | "image"
+  | "month"
+  | "number"
+  | "password"
+  | "radio"
+  | "range"
+  | "reset"
+  | "search"
+  | "submit"
+  | "tel"
+  | "text"
+  | "time"
+  | "url"
+  | "week";
+
+export type Component = {
+  id: string;
+  type: HTMLInputType;
+  options?: ComponentOptions;
+};
+
+type ComponentOptions = {
+  choices?: string[];
+  label?: string;
+};
