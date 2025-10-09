76b593f593d6a63158f904044234c0536fde11c0
Justin Rainier Go <81730861+jrgo7@users.noreply.github.com>
2025-10-09 12:10:03 +0800

    feat: add Form, Submission, Answer types

--- src/models/forms.ts
+++ src/models/forms.ts
@@ -22,8 +22,16 @@ type HTMLInputType =
   | "url"
   | "week";
 
+export type Form = {
+  id: string;
+  title: string;
+  userId: string;
+  createdAt: string;
+}
+
 export type Component = {
   id: string;
+  formId: string;
   type: HTMLInputType;
   options?: ComponentOptions;
 };
@@ -32,3 +40,21 @@ type ComponentOptions = {
   choices?: string[];
   label?: string;
 };
+
+export type Submission = {
+  id: string;
+  formId: string;
+  userId: string;
+  createdAt: string;
+}
+
+export type Answer = {
+  options?: AnswerOptions;
+  componentId: string;
+  submissionId: string;
+};
+
+type AnswerOptions = {
+  selected?: string[];
+  input?: string;
+}
