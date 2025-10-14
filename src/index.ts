aba0e0ea2aa6a7c145a97fa75af5793eb8bd55f3
Justin Rainier Go <81730861+jrgo7@users.noreply.github.com>
2025-10-14 12:55:26 +0800

    feat: register fastify-firebase plugin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import Fastify from 'fastify'
+import fastifyFirebase from 'fastify-firebase'
+import firebasePrivateKeyJson from '../neuron-delta-firebase-adminsdk-fbsvc-e748a310fb.json'
 import routes from './routes/sample'
 
 const fastify = Fastify({
   logger: true
 })
 
+fastify.register(fastifyFirebase, firebasePrivateKeyJson)
 fastify.register(routes)
 
 const start = async () => {
