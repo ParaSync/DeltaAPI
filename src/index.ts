28e11e33e8ac2b878935629e6d4c5c58d05c9691
nathaniel <nathaniel_irvin_adiong@dlsu.edu.ph>
2025-10-14 11:44:53 +0800

    feat: image and input logic

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import Fastify from 'fastify'
-import routes from './routes/sample'
+import Fastify from "fastify";
+import routes from "./routes/sample";
+import uploadRoutes from "./routes/upload";
+import componentRoutes from "./routes/components";
 
-const fastify = Fastify({
-  logger: true
-})
+const fastify = Fastify({ logger: true });
 
-fastify.register(routes)
+fastify.register(routes);
+fastify.register(uploadRoutes);
+fastify.register(componentRoutes);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000 });
+    console.log("Server running on http://localhost:3000");
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+
+start();
