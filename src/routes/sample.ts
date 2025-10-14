5b5aeed7a79af536bb4277d43c4c4a48ddd86336
Justin Rainier Go <81730861+jrgo7@users.noreply.github.com>
2025-10-14 12:55:26 +0800

    feat: register fastify-firebase plugin

--- src/routes/sample.ts
+++ src/routes/sample.ts
@@ -5,11 +5,11 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
  * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
  * @param {Object} options plugin options, refer to https://fastify.dev/docs/latest/Reference/Plugins/#plugin-options
  */
-async function routes (fastify: FastifyInstance, options: Object) {
+async function routes(fastify: FastifyInstance, options: Object) {
   fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     return { hello: 'world', note: 'try visiting /mirror!' }
   })
-  
+
   fastify.get('/mirror', async (request: FastifyRequest, reply: FastifyReply) => {
     return { request, reply }
   })
